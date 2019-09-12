eb66d4fdf48b882b23bc04111046e6a7815df4f2
alphajo <akshayjoshi0095@gmail.com>
2019-09-12 11:57:23 +0530

    uderstanding revert cmd

--- Learning.py
+++ Learning.py
@@ -231,5 +231,9 @@ My git clone url -	https://github.com/akshayjoshi0095/GIT_Learning.git
 The -d option stands for --delete, which would delete the local branch, only if you have already pushed and merged it with your remote branches.
 The -D option stands for --delete --force, which deletes the branch regardless of its push and merge status, so be careful using this one!
 
+## way to update any local branch with the master branch
+	> checkout to that branch
+	> $ git pull
+	> $ git merge master/any other branchname
 	
 '''
