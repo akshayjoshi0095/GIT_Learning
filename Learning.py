69e61f3f2f09af069f8261a41f8ed655e2afe103
alphajo <akshayjoshi0095@gmail.com>
2019-09-12 12:38:24 +0530

    trying stashing

--- Learning.py
+++ Learning.py
@@ -231,5 +231,8 @@ My git clone url -	https://github.com/akshayjoshi0095/GIT_Learning.git
 The -d option stands for --delete, which would delete the local branch, only if you have already pushed and merged it with your remote branches.
 The -D option stands for --delete --force, which deletes the branch regardless of its push and merge status, so be careful using this one!
 
+
+notthing to commit
+
 	
 '''
