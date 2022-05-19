3f93f794d18ae216365551c361450366d5fb7aea
akshayjoshi0095 <akshayjoshi0095@gmail.com>
2022-05-19 12:22:22 +0530

    changes 2nd

--- prog_interview.py
+++ prog_interview.py
@@ -47,3 +47,20 @@ sample_list = [10, 50, 50, 100, 100, 100, 20, 30, 40, 20, 30, 90, 40]
 
 print(bubble_sort(sample_list))
 
+"""
+Wap a program to remove all the element which are lesser than 5 in same list
+"""
+
+l1 = [0,1,2,3,4,5,6,7,8,9,0,1,1,2,3,4]
+def remove_list_elements(l):
+    for j in (l):
+        if j > 5:
+            t = l.index(j)
+            l.remove(j)
+            l.insert(t,"to_be_deleted")
+    while "to_be_deleted" in l:
+        l.remove("to_be_deleted")
+    print(l)
+remove_list_elements(l1)
+
+
